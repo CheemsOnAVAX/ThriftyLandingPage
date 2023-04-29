06f71e04d7ba23655c0312d9b9c43016ed3cd2b1
CheemsX devjr <103872426+CheemsOnAVAX@users.noreply.github.com>
2023-04-29 18:25:54 +0600

    Add files via upload

---
+++ dist/js/script.afc8b398.js
@@ -0,0 +1,13 @@
+// ======= Imports ======= //
+// ======= Variables ======= //
+// async function nftData() {
+//   const res = await fetch(
+//     "http://localhost:8080/getPutonsaleperpage?page=0&language=en&sortby=top"
+//   );
+//   const data = res.json();
+//   console.log(data);
+// }
+// ======= Codes ======= //
+// ======= Exports ======= //
+
+//# sourceMappingURL=script.afc8b398.js.map
