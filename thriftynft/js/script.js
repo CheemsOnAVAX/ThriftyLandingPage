4f9cd6e3cf9a7c0b91d39d38e682463297341bf0
CheemsX devjr <103872426+CheemsOnAVAX@users.noreply.github.com>
2023-04-29 17:03:13 +0600

    Add files via upload

---
+++ thriftynft/js/script.js
@@ -0,0 +1,16 @@
+// ======= Imports ======= //
+
+// ======= Variables ======= //
+
+// async function nftData() {
+//   const res = await fetch(
+//     "http://localhost:8080/getPutonsaleperpage?page=0&language=en&sortby=top"
+//   );
+//   const data = res.json();
+
+//   console.log(data);
+// }
+
+// ======= Codes ======= //
+
+// ======= Exports ======= //
